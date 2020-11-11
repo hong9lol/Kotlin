5c989a735bd282ce755f50a4924605206b679b46
Hong9lol <ljh890322@gmail.com>
2020-11-11 22:50:55 +0900

    11/11
    Signed-off-by: Hong9lol <ljh890322@gmail.com>

--- Finder/Finder/script.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.executeScript({
-    code:'document.querySelector("body").innerHTML'
-}, function(result){
-    alert("hello");
-});
--- Finder/src/main/kotlin/main.kt
+++ Finder/src/main/kotlin/main.kt
@@ -1,11 +1,35 @@
+package domain
+import org.w3c.dom.events.Event
+import kotlin.browser.document
 import kotlinx.browser.document
+
+external val bodyText: dynamic
+external fun init()
+external fun alert(arg: dynamic)
+external fun startProcess(arg: String)
 
 fun main() {
     document.write("Hello, world!")
-    js("chrome.tabs.executeScript({\n" +
-            "    code:'document.querySelector(\"body\").innerHTML'\n" +
-            "}, function(result){\n" +
-            "    alert(\"hello\");\n" +
-            "});");
+    init()
 
+
+    fun test(arg: dynamic) {
+        alert(arg)
+    }
+
+//    js("alert(\"OK!!!\");")
+//    js("chrome.tabs.executeScript({\n" +
+//            "    code:'document.querySelector(\"body\").innerHTML'\n" +
+//            "}, function(result){\n" +//            "    alert(\"hello\");\n" +
+//            "});");
+
+    alert(bodyText)
 }
+
+fun selectDOMNode(){
+    val saveButton= document.getElementById("saveBtn")
+//    saveButton?.addEventListener("click”, save)
+    // saveButton?.addEventListener("click”, { alert(“on click event”} ….. 이렇게도 가능할거 같음
+}
+
+//https://www.slipp.net/wiki/pages/viewpage.action?pageId=30769702
