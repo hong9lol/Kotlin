f2e70e9fa33aca3b0f71da3cc1625a0106382ff0
Hong9lol <ljh890322@gmail.com>
2020-11-22 23:37:01 +0900

    2020 11 21
    Signed-off-by: Hong9lol <ljh890322@gmail.com>

---
+++ Finder/Finder/script.js
@@ -0,0 +1,10 @@
+var bodyText = null;
+function init() {
+chrome.tabs.executeScript({
+    code:'document.querySelector("body").innerHTML;'
+}, function(result){
+
+        var user = this['kotlin-js'].domain.User.prototype.study2();
+});
+
+}
--- Finder/src/main/kotlin/main.kt
+++ Finder/src/main/kotlin/main.kt
@@ -1,35 +1,29 @@
 package domain
-import org.w3c.dom.events.Event
-import kotlin.browser.document
+import chrome.tabs.InjectDetails
+import chrome.tabs.executeScript
 import kotlinx.browser.document
+import kotlinjs.common.jsonAs
 
-external val bodyText: dynamic
-external fun init()
 external fun alert(arg: dynamic)
-external fun startProcess(arg: String)
 
 fun main() {
-    document.write("Hello, world!")
-    init()
 
-
-    fun test(arg: dynamic) {
-        alert(arg)
+    executeScript(jsonAs<InjectDetails>().apply {
+        code = document.querySelector("body")?.baseURI
+    }) {
+        result -> alert(result)
     }
+
 
 //    js("alert(\"OK!!!\");")
 //    js("chrome.tabs.executeScript({\n" +
 //            "    code:'document.querySelector(\"body\").innerHTML'\n" +
 //            "}, function(result){\n" +//            "    alert(\"hello\");\n" +
 //            "});");
-
-    alert(bodyText)
 }
 
-fun selectDOMNode(){
-    val saveButton= document.getElementById("saveBtn")
-//    saveButton?.addEventListener("click”, save)
-    // saveButton?.addEventListener("click”, { alert(“on click event”} ….. 이렇게도 가능할거 같음
-}
-
-//https://www.slipp.net/wiki/pages/viewpage.action?pageId=30769702
+//fun selectDOMNode(){
+//    val saveButton= document.getElementById("saveBtn")
+////    saveButton?.addEventListener("click”, save)
+//    // saveButton?.addEventListener("click”, { alert(“on click event”} ….. 이렇게도 가능할거 같음
+//}
